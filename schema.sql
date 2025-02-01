a1f9459ff46acd1e78e577ea75f6f656378a606a
Akhilandeshwari Puppala <akhilandeshwariv@gmail.com>
2025-02-01 15:47:42 +0530

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -1,12 +1,10 @@
-CREATE TABLE team_player_details (
+CREATE TABLE team_players (
     id INT AUTO_INCREMENT PRIMARY KEY,
     team_id INT NOT NULL,
     player_id INT NOT NULL,
     player_runs INT DEFAULT 0,
     player_wickets INT DEFAULT 0,
     player_catches INT DEFAULT 0,
-    player_balls_faced INT DEFAULT 0,
-    player_balls_delivered INT DEFAULT 0,
-    catches INT DEFAULT 0
+    FOREIGN KEY (team_id) REFERENCES teams(id),
+    FOREIGN KEY (player_id) REFERENCES players(id)
 );
-
